1fa7f8912e8234d0f81e7f1e93594fd5642340be
emigon <emipastor2001@gmail.com>
2023-11-20 14:32:16 -0300

    Se finalizo la tarea

--- init.js
+++ init.js
@@ -1,49 +1,59 @@
 const form = document.querySelector('#formulario');
+const warning = document.querySelector('.warning');
+sessionStorage.setItem('terminos','false');
 
 function checkPassword(){
     const password1 = document.querySelector("#password1").value;
     const password2 = document.querySelector("#password2").value;
     let msj = document.getElementById("mensaje");
     if(password1 !== password2){
-        msj.textContent = 'las contraseñas no coinciden'
+        msj.textContent = 'Las contraseñas no coinciden'
+        msj.classList.remove('d-none')
     }
     else{
         form.classList.add('was-validated');
+        msj.classList.add('d-none');
         
     }
 }
 
 function terminos(){
-    const check = document.getElementById('terminos')
+    const check = document.getElementById('terminos');
     check.addEventListener('input',()=>{
         if(check.checked){
             console.log("true")
-            localStorage.setItem('terminos','true');
+            sessionStorage.setItem('terminos','true');
             
         }
         else{
             console.log("false")
-            localStorage.setItem('terminos','false');
+            sessionStorage.setItem('terminos','false');
+            
             
         }
     })
 }
 
 function validar(){
-    let localCheck = localStorage.getItem('terminos')
+    let localCheck = sessionStorage.getItem('terminos')
     if(form.checkValidity() && localCheck == 'true'){
-        alert("formulario enviado")
         window.location.reload;
-        console.log("joya")
+        warning.classList.add('d-none')
+        console.log("pasa")
+    }
+    if(form.checkValidity() && localCheck == 'false'){
+        warning.classList.remove('d-none')
+        e.preventDefault()
     }
     else{
+        e.preventDefault()
         alert("Complete todos los campos")
         console.log("no pasa")
     }
 }
 
 
-form.addEventListener('submit',()=>{
+form.addEventListener('submit',(e)=>{
     checkPassword()
     validar()
 })
